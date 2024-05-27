14601436f83f02d91613f777ab3a885085ca4762
jeongwon <tjtkdvl@gmail.com>
2024-05-27 17:48:40 +0900

    #4. QueryDSL의 다양한 문법들(동적 SQL 활용)

--- src/main/java/com/example/study/repository/MemberRepositoryCustom.java
+++ src/main/java/com/example/study/repository/MemberRepositoryCustom.java
@@ -9,4 +9,6 @@ public interface MemberRepositoryCustom {
     // JPA 메서드가 아님. (JpaRepository 인터페이스 상속받지 않음)
     List<Member> findByName(String name);
 
+    List<Member> findUser(String nameParam, int ageParam);
+
 }
